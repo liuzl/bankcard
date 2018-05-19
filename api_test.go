45df96711218ca1df5a50551d74b0f64778fb1a8
liuzhanliang <zhanliangliu@qq.com>
2018-05-19 12:07:55 +0800

    test cases

--- api_test.go
+++ api_test.go
@@ -5,7 +5,8 @@ import (
 )
 
 func TestFindBank(t *testing.T) {
-	b, err := FindBank("527594112231")
+	//b, err := FindBank("527594112231")
+	b, err := FindBank("6222000200124846494")
 	if err != nil {
 		t.Error(err)
 	}
